1714d110da9cc449870960b5b691f4a23c6c25e1
Tolstouhov <ykt94@yandex.ru>
2023-05-01 11:26:35 +0300

    fix: test env

---
+++ tests/MemcachedClientTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MemcachedClientTest extends TestCase
+{
+    public function testSet(): void
+    {
+
+    }
+
+    public function testGet(): void
+    {
+
+    }
+
+    public function testDelete(): void
+    {
+
+    }
+}
